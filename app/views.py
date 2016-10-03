f0492aa0c2ce21d03522acc6369378239feed7c3
Nicolas Leblanc <nicolas.leblanc@schneider-electric.com>
2016-10-03 18:27:04 -0400

    fixed encoding to latin1

--- app/views.py
+++ app/views.py
@@ -6,6 +6,6 @@ from app import app
 @app.route('/index')
 def index():
     url = 'http://donnees.ville.montreal.qc.ca/dataset/772087ee-e11f-42a8-ab6b-a175d6c6e249/resource/9d77d85a-f54c-408a-b6ff-3d847c05b2e0/download/sause01direction01directiondonneesouvertesbatimentsvacantsbatimentsvacantsvm2015.csv'
-    buildings = pd.read_csv(url, sep = ',')
+    buildings = pd.read_csv(url, sep = ',',encoding = "latin1")
     return render_template('index.html',
                           buildings=buildings)
