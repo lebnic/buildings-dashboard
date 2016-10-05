b1eec280571130448965c308c6012fc4c21ec2af
Nicolas Leblanc <nicolas.leblanc@schneider-electric.com>
2016-10-05 12:07:53 -0400

    added more info to infowindow

--- app/views.py
+++ app/views.py
@@ -7,5 +7,6 @@ from app import app
 def index():
     url = 'http://donnees.ville.montreal.qc.ca/dataset/772087ee-e11f-42a8-ab6b-a175d6c6e249/resource/9d77d85a-f54c-408a-b6ff-3d847c05b2e0/download/sause01direction01directiondonneesouvertesbatimentsvacantsbatimentsvacantsvm2015.csv'
     buildings = pd.read_csv(url, sep = ',',encoding = "latin1")
+    buildings['VALEUR_TOTAL'] = buildings['VALEUR_TERRAIN']+buildings['VALEUR_BATIMENT']
     return render_template('index.html',
-                          buildings=buildings)
+                          buildings=buildings.sort(['VALEUR_TOTAL'], ascending=[True]))
